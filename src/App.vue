e1ac9574b5ce36afbbe0e8bc0b4030e8900aa969
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2024-11-16 01:25:52 +0600

    life cycle hook

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { ref, reactive } from "vue";
+import LifeCycleHookComponent from "./components/LifeCycleHookComponent.vue";
+
+const count = ref({
+  amount: 100,
+  income: {
+    job: 100,
+    freelance: 50,
+    side_project: 20,
+  },
+});
+const state = reactive({ count: 0 });
+
+console.log("js portion state 1:", state.count);
+
+function counterHandler() {
+  count.value = {
+    amount: 1200,
+    income: {
+      job: 1200,
+      freelance: 520,
+      side_project: 220,
+    },
+  };
+}
+
+function reactiveStateHandler() {
+  state.count += 10;
+}
+</script>
+
+<template class="border border-red-500">
+  <div class="w-full max-w-[1024px] mx-auto bg-green-500 py-6 px-4">
+    <h2 class="text-3xl text-red-500">This is first vue component</h2>
+    <h6>Freelance Income: {{ count.income.freelance }}</h6>
+    <button
+      @click="counterHandler"
+      class="px-5 py-3 border border-green-300 rounded-full"
+    >
+      Count {{ count.amount }}
+    </button>
+    <button
+      @click="reactiveStateHandler"
+      class="mx-3 px-5 py-3 border border-green-300 rounded-full"
+    >
+      Reactive Count {{ state.count }}
+    </button>
+
+    <div>
+      <div v-if="state.count < 20">
+        <LifeCycleHookComponent />
+      </div>
+      <div v-else><h2>LifeCycleHookComponent is unmounted</h2></div>
+    </div>
+  </div>
+</template>
---
+++ src/components/LifeCycleHookComponent.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from "vue";
+
+const count = ref(0);
+
+onBeforeMount(() => {
+  console.log("Before Mount Hook is called");
+});
+
+onMounted(() => {
+  console.log("Mount Hook is called");
+});
+
+onBeforeUpdate(() => {
+  console.log("Before Update Hook is called");
+});
+
+onUpdated(() => {
+  console.log("Updated Hook is called");
+});
+
+onBeforeUnmount(() => {
+  console.log("Before Unmount Hook is called");
+});
+
+onUnmounted(() => {
+  console.log("Unmounted Hook is called");
+});
+</script>
+
+<template>
+  <div>
+    <h2>Practice Life Cycle Hook's of Vue Js</h2>
+    <button class="border border-red-300 px-4 py-2 rounded-xl" @click="count++">
+      {{ count }}
+    </button>
+  </div>
+</template>
